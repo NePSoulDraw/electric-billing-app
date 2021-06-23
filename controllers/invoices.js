2eff356d0cfe54e58fa61f0ea4454d059c2cd03f
Ismael Ahumada <nepsouldraw@gmail.com>
2021-06-23 20:19:03 +0200

    Invoice model and controller created

--- controllers/invoices.js
+++ controllers/invoices.js
@@ -1,42 +1,101 @@
-
-const InvoicesGet = async() => {
-
-
+const { response, request } = require('express');
+const Invoice = require('../models/invoice');
 
 
+const InvoicesGet = async( req=request, res=response ) => {
 
-}
+    [total, invoices] = await Promise.all([
+        await Invoice.countDocuments(),
+        await Invoice.find()
+    ]);
 
-const InvoiceGet = async() => {
-
-
-
-
-
-}
-
-const InvoicePost = async() => {
-
-
-
-
+    res.status(200).json({
+        total,
+        invoices
+    });
 
 }
 
-const InvoicePut = async() => {
+const InvoiceGet = async( req=request, res=response ) => {
 
+    const { id } = req.params;
 
+    if (!id){
+        res.status(400).json({
+            msg: "La factura no existe"
+        });
+    }
 
+    const invoice = await Invoice.findById(id);
 
+    res.status(200).json({
+        invoice
+    });
 
 }
 
-const InvoiceDelete = async() => {
+const InvoicePost = async( req=request, res=response ) => {
 
+    const { fecha, hora, consumo, precio, costeHora } = req.body;
 
+    const invoice = new Invoice({ 
+     "Fecha": fecha,
+     "Hora": hora,
+     "Consumo (Wh)": consumo,
+     "Precio (€/kWh)": precio,
+     "Coste por hora (€)": costeHora });
 
+     await invoice.save();
 
+     res.json({
+         invoice
+     });
 
+}
+
+const InvoicePut = async( req=request, res=response ) => {
+
+    const invoice_id = req.params.id;
+    const { fecha, hora, consumo, precio, costeHora } = req.body;
+
+    const data = { 
+        "Fecha": fecha,
+        "Hora": hora,
+        "Consumo (Wh)": consumo,
+        "Precio (€/kWh)": precio,
+        "Coste por hora (€)": costeHora };
+
+    if(!invoice_id){
+        res.status(400).json({
+            msg: "La factura no existe"
+        });
+    }
+
+     const updated_invoice = await Invoice.findByIdAndUpdate(invoice_id, data, {new: true});
+
+     res.status(200).json({
+         updated_invoice
+     });
+
+}
+
+const InvoiceDelete = async( req=request, res=response ) => {
+
+    const invoice_id = req.params.id;
+
+    const invoiceExists = await Invoice.findById( invoice_id );
+
+    if ( !invoiceExists ){
+        return res.status(400).json({
+            msg: 'La factura no existe, no se ha podido eliminar'
+        });
+    }
+
+    await Invoice.findByIdAndRemove(invoice_id);
+
+    res.status(200).json({
+        msg: "Factura eliminada correctamente"
+    });
 }
 
 module.exports = {
--- models/invoice.js
+++ models/invoice.js
@@ -1,13 +1,42 @@
 const { model, Schema } = require('mongoose');
 
-const InvoiceModel = Schema({
+const InvoiceSchema = Schema({
+    // MongoDB no soporta los elementos como el "." o "$",
+    // por lo tanto he decidido dejarlos como String
 
+    "Fecha": {
+        type: String,
+        required: true
+    },
 
+    "Hora": {
+        type: String,
+        required: true
+    },
 
+    "Consumo (Wh)": {
+        type: String,
+        required: true
+    },
 
+    "Precio (€/kWh)": {
+        type: String,
+        required: true
+    },
 
-
-
-
+    "Coste por hora (€)": {
+        type: String,
+        required: true
+    }
 
 });
+
+InvoiceSchema.methods.toJSON = function() {
+
+    const { __v, _id, ...invoice } = this.toObject();
+    invoice.id = _id;
+    return invoice;
+
+}
+
+module.exports = model('Invoice', InvoiceSchema);
