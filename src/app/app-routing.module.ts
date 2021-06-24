e3f13f79b342baeb1969d375e2a853cd7bb727a8
Ismael Ahumada <nepsouldraw@gmail.com>
2021-06-24 21:48:24 +0200

    Files and main structure prepared for coding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { DetallesComponent } from './pages/detalles/detalles.component';
+
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'detalles/:id', component: DetallesComponent },
+  { path: '**', pathMatch: 'full', redirectTo: '/inicio' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { DetallesComponent } from './pages/detalles/detalles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    InicioComponent,
+    DetallesComponent
   ],
   imports: [
     BrowserModule,
